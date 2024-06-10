11ad6551724c7545813a6d4f324598ea03a8efc9
ZabihullahNooriWardak <zabinoori712@gmail.com>
2024-06-10 11:41:31 +0430

    fixed bug

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,7 +1,7 @@
 class Admin::OrdersController < ApplicationController
   before_action :authenticate_user!
   before_action :ensure_admin
-
+  rescue_from ActiveRecord::RecordNotFound, with: :order_not_found
   def index
     @orders = Order.all
   end
@@ -33,5 +33,9 @@ class Admin::OrdersController < ApplicationController
 
   def order_params
     params.require(:order).permit(:status)
+  end
+  def order_not_found
+    flash[:alert] = "Order not found."
+    redirect_to admin_orders_path
   end
 end
