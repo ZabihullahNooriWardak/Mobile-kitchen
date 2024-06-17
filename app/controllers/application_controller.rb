8b52a50c7529afffddb00a60682b7dca09340f9f
ZabihullahNooriWardak <zabinoori712@gmail.com>
2024-06-17 14:33:15 +0430

    adds counter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+    
+    before_action :set_cart
+
+    private
+  
+    def set_cart
+      @cart = current_user.cart if user_signed_in?
+    end
 end
