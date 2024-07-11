1e187dc37f455bac65315e3eddb848542a29d2b5
ZabihullahNooriWardak <zabinoori712@gmail.com>
2024-07-11 08:57:45 +0430

    fixed

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -2,6 +2,13 @@ class CartItemsController < ApplicationController
   before_action :authenticate_user!
 
   def create
+    @food = Food.find_by(id: params[:food_id])
+    
+    if @food.nil?
+      redirect_to foods_path, alert: 'The food item you are trying to add does not exist.'
+      return
+    end
+
     @cart = current_user.cart || current_user.create_cart
     @cart_item = @cart.cart_items.find_or_initialize_by(food_id: params[:food_id])
 
