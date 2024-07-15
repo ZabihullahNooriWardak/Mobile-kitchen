09d5fbaae141b9f5e1759cf525f2a280aae16e1e
ZabihullahNooriWardak <zabinoori712@gmail.com>
2024-07-15 08:57:45 +0430

    made some improvements

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,11 @@
 # app/controllers/reviews_controller.rb
 class ReviewsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_order
+  before_action :set_order, only: [:create]
+  # before_action :set_order  
+  def index
+@reviews= Review.all
+  end
 
   def create
     @review = @order.build_review(review_params.merge(user: current_user))
@@ -15,11 +19,15 @@ class ReviewsController < ApplicationController
 
   private
 
-  def set_order
-    @order = Order.find(params[:order_id])
-  end
+  # def set_order
+  #   @order = Order.find(params[:order_id])
+  # end
 
   def review_params
     params.require(:review).permit(:rating, :feedback, :suggestion)
+  end
+
+  def set_order
+    @order = Order.find(params[:order_id])
   end
 end
