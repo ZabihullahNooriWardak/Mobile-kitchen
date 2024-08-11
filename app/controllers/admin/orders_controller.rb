2758908850587be10cba9338b1f6fbcc8a61a4af
ZabihullahNooriWardak <zabinoori712@gmail.com>
2024-08-11 17:05:59 +0430

    update the code and adds css

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -2,8 +2,9 @@ class Admin::OrdersController < ApplicationController
   before_action :authenticate_user!
   before_action :ensure_admin
   rescue_from ActiveRecord::RecordNotFound, with: :order_not_found
+
   def index
-    @orders = Order.all
+    @orders = Order.order(created_at: :desc)
   end
 
   def show
@@ -38,6 +39,7 @@ class Admin::OrdersController < ApplicationController
   def order_params
     params.require(:order).permit(:status)
   end
+
   def order_not_found
     flash[:alert] = "Order not found."
     redirect_to admin_orders_path
